3e0e786da4a810001d3750b9ead392324282842f
Gianvito <gianvitotortomasi@gmail.com>
2020-03-26 04:56:19 +0100

    Indentato codice

--- background.js
+++ background.js
@@ -1,15 +1,14 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([
-        {
-          conditions: [
-            new chrome.declarativeContent.PageStateMatcher({
-              pageUrl: { urlContains: "https://offweb.unipa.it/offweb/public/aula/weekCalendar.seam" },
-            })
-          ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-        }
-      ]);
-    });
-  }
-);
+chrome.runtime.onInstalled.addListener(function () {
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+		chrome.declarativeContent.onPageChanged.addRules([{
+			conditions: [
+				new chrome.declarativeContent.PageStateMatcher({
+					pageUrl: {
+						urlContains: "https://offweb.unipa.it/offweb/public/aula/weekCalendar.seam"
+					},
+				})
+			],
+			actions: [new chrome.declarativeContent.ShowPageAction()]
+		}]);
+	});
+});
--- content.js
+++ content.js
@@ -1,40 +1,30 @@
 var search = document.getElementById("frc:search");
 var print = "<span id=\"printBtn__\" style=\"margin-left:30px;\" class=\"btn btn-primary btn-sm\">Stampa</span>";
 
-search.insertAdjacentHTML('afterEnd',print);
+search.insertAdjacentHTML('afterEnd', print);
 
 document.getElementById("printBtn__").addEventListener("click", () => {
-  var printStyle = document.createElement("style");
-  printStyle.type="text/css";
-  printStyle.id="printStyle";
-  printStyle.href = "style.css";
-  removePrintStyles();
-  document.head.appendChild(printStyle);
+	var printStyle = document.createElement("style");
+	printStyle.type = "text/css";
+	printStyle.id = "printStyle";
+	printStyle.href = "style.css";
+	removePrintStyles();
+	document.head.appendChild(printStyle);
 
-  var printScript = document.createElement("script");
-  printScript.type = "text/javascript";
-  printScript.id = "printScript";
-  printScript.innerHTML = "window.print()";
-  document.head.appendChild(printScript);
-  document.head.removeChild(document.getElementById("printScript"));
-  document.head.removeChild(document.getElementById("printStyle"));
+	var printScript = document.createElement("script");
+	printScript.type = "text/javascript";
+	printScript.id = "printScript";
+	printScript.innerHTML = "window.print()";
+	document.head.appendChild(printScript);
+	document.head.removeChild(document.getElementById("printScript"));
+	document.head.removeChild(document.getElementById("printStyle"));
 });
 
-function removePrintStyles(){
-  var links = document.getElementsByTagName("link");
-  for(link of links){
-    if(link.media == "print"){
-      link.parentNode.removeChild(link);
-    }
-  }
+function removePrintStyles() {
+	var links = document.getElementsByTagName("link");
+	for (link of links) {
+		if (link.media == "print") {
+			link.parentNode.removeChild(link);
+		}
+	}
 }
-
-/*function renameDays(){
-  var daysIT = ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì"];
-  var days = document.getElementsByClassName("fc-day-header").innerHTML;
-  if(daysIT.length == days.length){
-    for(var i = 0; i < days.length; i++){
-      days[i].innerHTML = daysIT[i];
-    }
-  }
-}*/
